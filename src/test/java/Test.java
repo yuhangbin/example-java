afebb590ae1221ec2c113ca8d8eb0ea98eeed118
yuhangbin <yuhangbinccc@gmail.com>
2021-12-04 17:58:48 +0800

    Just test something.

--- src/test/java/Test.java
+++ src/test/java/Test.java
@@ -1,8 +1,6 @@
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.Iterator;
-import java.util.LinkedList;
-import java.util.List;
+import java.util.*;
 import java.util.function.BiConsumer;
 
 /**
@@ -10,31 +8,24 @@ import java.util.function.BiConsumer;
  * @date 2021/6/30
  **/
 @Slf4j
-public class Test<T extends Integer,U extends Integer> {
-    public T t;
-    public U u;
-
-    public Test(T t, U u) {
-        this.t = t;
-        this.u = u;
-    }
+public class Test{
 
     public static void main(String[] args) {
-        List<Integer> list = new LinkedList<>();
-        list.add(1);
-        list.add(2);
-        list.add(2);
-        Iterator<Integer> integers = list.iterator();
-        while (integers.hasNext()){
-            integers.remove();
-            System.out.println("remove: " + integers.next());
+        List<Thread> threads = new LinkedList<>();
+        for (int i = 0; i < 10; i++) {
+            final Integer num = i;
+            threads.add(new Thread(() -> {
+                System.out.println(num);
+            }));
         }
-        System.out.println(list.size());
+        threads.forEach(x -> {
+            x.start();
+            try {
+                x.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
     }
-    private void functionA(BiConsumer<T, U> consumer) {
-            consumer.andThen(((t, u) -> {
-                System.out.println("then x: " + t);
-                System.out.println("then y: " + u);
-            })).accept(t, u);
-    }
+    
 }

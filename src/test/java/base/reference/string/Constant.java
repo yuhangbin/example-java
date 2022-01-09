10bdc04de36c4406de4c2cfd0f612c2f5aaee63b
yuhangbin <yuhangbinccc@gmail.com>
2022-01-09 22:26:17 +0800

    StringReferenceTest.

---
+++ src/test/java/base/reference/string/Constant.java
@@ -0,0 +1,14 @@
+package base.reference.string;
+
+import lombok.Data;
+
+/**
+ * @author yuhangbin
+ * @date 2022/1/5
+ **/
+@Data
+public class Constant {
+
+	public static String value = "default";
+
+}
